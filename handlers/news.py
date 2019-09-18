40521e9a4e9674c108220488e31f4e32234bac55
Aleksei Chusovliankin <lixich@mail.ru>
2019-09-19 02:45:47 +0500

    add get posts with filters

--- handlers/news.py
+++ handlers/news.py
@@ -1,22 +1,41 @@
 import json
-from flask import Blueprint, jsonify, request, make_response
-from playhouse.shortcuts import model_to_dict, dict_to_model
+from flask import Blueprint, jsonify, request
 from models import NewsModel
 from .error import generate_error
 
 app_url = Blueprint('', __name__)
 
 
-@app_url.route('/', methods = ['GET'])
+@app_url.route('/', methods=['GET'])
 def get_news():
-    order = request.args.get('order') if 'order' in request.args else None
-    desc = request.args.get('desc') if 'desc' in request.args else False
-    limit = request.args.get('limit') if 'limit' in request.args else None
-    offset = request.args.get('offset') if 'offset' in request.args else None
+    order = str(request.args.get('order')) if 'order' in request.args else 'id'
+    desc = str(request.args.get('desc')).lower() if 'desc' in request.args else 'false'
+    limit = str(request.args.get('limit')) if 'limit' in request.args else '5'
+    offset = str(request.args.get('offset')) if 'offset' in request.args else None
+
     if order not in NewsModel.get_fields():
         return generate_error(400, 'Field "order" is incorrect')
-    if desc not in (False, True):
+    order_field = getattr(NewsModel, order)
+    if desc not in ('false', 'true'):
         return generate_error(400, 'Field "desc" is incorrect')
-    news_models = NewsModel.select().get()
-    json_data = json.dumps(model_to_dict(news_models))
-    return jsonify(json_data)
+    if desc == 'true':
+        order_field = order_field.desc()
+    if limit and not limit.isnumeric():
+        return generate_error(400, 'Field "limit" is incorrect')
+    if limit:
+        limit = int(limit)
+        if limit > 500:
+            return generate_error(400, '"limit" is too long')
+    if offset and not offset.isnumeric():
+        return generate_error(400, 'Field "offset" is incorrect')
+    if offset:
+        offset = int(offset)
+    query = NewsModel.select().order_by(order_field)
+    if offset:
+        query = query.offset(offset)
+    if limit:
+        query = query.limit(limit)
+
+    news_dicts = list(query.dicts())
+
+    return jsonify(json.dumps(news_dicts))
